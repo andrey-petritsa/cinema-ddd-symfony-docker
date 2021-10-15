c2066bdfb2c62e249db1a0e5836be03f21a6c2bd
andrey-petritsa <andrey@petritsa.ru>
2021-10-15 17:15:16 +0300

    I-10 Datamap with Doctrine

--- app/src/Domain/Booking/Entity/Session/Ticket.php
+++ app/src/Domain/Booking/Entity/Session/Ticket.php
@@ -3,11 +3,29 @@
 namespace App\Domain\Booking\Entity\Session;
 
 use App\Domain\Booking\ValueObject\ClientDetails;
+use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\UuidInterface;
 
+/** @ORM\Entity */
 class Ticket
 {
-    public function __construct(private UuidInterface $id, private Session $session, private ClientDetails $clientDetails)
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Column(type="integer")
+     */
+    private UuidInterface $id;
+
+    /** @ORM\ManyToOne(targetEntity="Session") */
+    private Session $session;
+
+    /** @Orm\Embedded(class="App\Domain\Booking\ValueObject\ClientDetails") */
+    private ClientDetails $clientDetails;
+
+    public function __construct(UuidInterface $id, Session $session, ClientDetails $clientDetails)
     {
+        $this->id = $id;
+        $this->session = $session;
+        $this->clientDetails = $clientDetails;
     }
 }
