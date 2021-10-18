4c88a16f50db4c061ac0e9b829b66e5be1018a13
andrey-petritsa <andrey@petritsa.ru>
2021-10-18 10:28:04 +0300

    I-8 Add service for working with movie with method to create movie

---
+++ app/src/Controller/MovieController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\MovieService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends AbstractController
+{
+    public function __construct(private MovieService $movieService)
+    {
+    }
+
+    #[Route('/movie', methods: ['POST'])]
+    public function createMovie(Request $request): Response
+    {
+        $requestBody = $request->toArray();
+        $movie = $this->movieService->createMovie($requestBody['name'], $requestBody['duration']);
+
+        return $this->json(['id' => $movie->getId()]);
+    }
+
+    #[Route( '/movie', methods: ['GET'] )]
+    public function getMovies (Request $request): Response
+    {
+        return $this->json([
+            'name' => 'hello'
+        ]);
+    }
+
+}
---
+++ app/src/Service/MovieService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service;
+
+use App\Domain\Booking\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
+use Ramsey\Uuid\Uuid;
+
+class MovieService
+{
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+
+    public function createMovie(string $movieName, string $rawMovieDuration)
+    {
+        $movieDuration = new \DateInterval($rawMovieDuration);
+        $movie = new Movie($movieName, $movieDuration);
+
+        $this->entityManager->persist($movie);
+        $this->entityManager->flush();
+
+        return $movie;
+    }
+}
