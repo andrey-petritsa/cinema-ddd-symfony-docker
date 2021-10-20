d2afed33a65ad3521b09abcbd7d4601a61587c68
andrey-petritsa <andrey@petritsa.ru>
2021-10-20 12:35:35 +0300

    I-5 add controller for booking page

--- app/src/Controller/Page/BookingPageController.php
+++ app/src/Controller/Page/BookingPageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Page;
 
+use App\Domain\Booking\Entity\Session\Session;
+use App\Repository\SessionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,9 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BookingPageController extends AbstractController
 {
-    #[Route( '/', methods: ['GET'] )]
-    public function index(Request $request): Response
+    #[Route('/', methods: ['GET'])]
+    public function index(Request $request, SessionRepository $sessionRepository): Response
     {
-        return new Response();
+        return $this->render('page/booking.html.twig', [
+            'sessions' => $sessionRepository->findAll()
+        ]);
     }
 }
