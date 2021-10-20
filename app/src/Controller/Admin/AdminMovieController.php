91618274dd1c1f98b7e317317e046a8687e9c404
andrey-petritsa <andrey@petritsa.ru>
2021-10-20 10:03:57 +0300

    I-7 add change movie page

---
+++ app/src/Controller/Admin/AdminMovieController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Command\Movie\ChangeMovie\ChangeMovieCommand;
+use App\Command\Movie\CreateMovie\CreateMovieCommand;
+use App\Domain\Booking\Entity\Movie;
+use App\Form\MovieType;
+use App\Repository\MovieRepository;
+use Ramsey\Uuid\Uuid;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminMovieController extends AbstractController
+{
+    #[Route('/admin/movie/create', methods: ['GET', 'POST'], name: "admin_create_movie")]
+    public function createMovie(Request $request, MovieRepository $movieRepository): Response
+    {
+        $createMovieCommand = new CreateMovieCommand(Uuid::uuid4(), 'Название фильма', "PT2H25M");
+        $createMovieForm = $this->createForm(MovieType::class, $createMovieCommand);
+        $createMovieForm->handleRequest($request);
+
+        if ($createMovieForm->isSubmitted() && $createMovieForm->isValid()) {
+            $this->dispatchMessage($createMovieCommand);
+        }
+
+        return $this->renderForm('admin/movie/movie.create.twig.html', [
+            'createMovieForm' => $createMovieForm,
+            'movies' => $movieRepository->findAll()
+        ]);
+    }
+
+    #[Route('/admin/movie/change/{id}', methods: ['GET', 'POST'], name: 'admin_change_movie')]
+    public function changeMovie(Request $request, Movie $movie): Response
+    {
+        $changeMovieCommand = new ChangeMovieCommand($movie->getId(), $movie->getName(), $movie->getDuration()->format('PT%hH%iM'));
+        $changeMovieForm = $this->createForm(MovieType::class, $changeMovieCommand);
+        $changeMovieForm->handleRequest($request);
+
+        if ($changeMovieForm->isSubmitted() && $changeMovieForm->isValid()) {
+            $this->dispatchMessage($changeMovieCommand);
+        }
+
+        return $this->renderForm('admin/movie/movie.change.twig.html', [
+            'changeMovieForm' => $changeMovieForm
+        ]);
+    }
+}
