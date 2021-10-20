2e5f65c0f00658843ca87f41e0de352678fd8ece
andrey-petritsa <andrey@petritsa.ru>
2021-10-20 09:02:19 +0300

    I-7 add simple form mapping with create movie command

--- app/src/Controller/Page/AdminController.php
+++ app/src/Controller/Page/AdminController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller\Page;
 
+use App\Command\Movie\CreateMovie\CreateMovieCommand;
 use App\Repository\MovieRepository;
+use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\MovieType;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin/movie', methods: ['GET'])]
     public function index(MovieRepository $movieRepository): Response
     {
-        return $this->render('admin/movie.twig.html', [
-            'movies' => $movieRepository->findAll()
+        $createMovieCommand = new CreateMovieCommand(Uuid::uuid4(), 'Название фильма', "PT2H25M");
+        $form = $this->createForm(MovieType::class, $createMovieCommand);
+
+        return $this->renderForm('admin/movie.twig.html', [
+            'form' => $form
         ]);
     }
 }
