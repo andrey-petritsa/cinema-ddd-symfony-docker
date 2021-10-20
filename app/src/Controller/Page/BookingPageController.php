e920cf8ec65c170e126d7e9173f607d53cb7081d
andrey-petritsa <andrey@petritsa.ru>
2021-10-20 15:25:22 +0300

    I-5 add page for change session

--- app/src/Controller/Page/BookingPageController.php
+++ app/src/Controller/Page/BookingPageController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Page;
 
+use App\Command\DomainCases\BookTicket\BookTicketCommand;
 use App\Domain\Booking\Entity\Session\Session;
+use App\Form\UserType;
 use App\Repository\SessionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,11 +13,30 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BookingPageController extends AbstractController
 {
-    #[Route('/', methods: ['GET'])]
+    //QUESTION можно ли было сделать как в макете? Форму на этой же странице
+    #[Route('/', methods: ['GET'], name: 'user_show_sessions')]
     public function index(Request $request, SessionRepository $sessionRepository): Response
     {
-        return $this->render('page/booking.html.twig', [
-            'sessions' => $sessionRepository->findAll()
+        return $this->render('page/index.html.twig', [
+            'sessions' => $sessionRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/session/book/{id}', methods: ['GET', 'POST'], name: 'user_book_ticket')]
+    public function bookPage(Session $session, Request $request)
+    {
+        $bookTicketCommand = new BookTicketCommand($session->getId());
+        $form = $this->createForm(UserType::class, $bookTicketCommand);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->dispatchMessage($bookTicketCommand);
+            $this->addFlash('success', 'Билет был зарезервирован');
+            return $this->redirectToRoute('user_show_sessions');
+        }
+
+        return $this->renderForm('page/booking.html.twig', [
+            'form' => $form,
         ]);
     }
 }
---
+++ app/src/Form/UserType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Form;
+
+use App\Domain\Booking\Entity\Movie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('phone', TelType::class)
+            ->add('submit', SubmitType::class);
+    }
+}
