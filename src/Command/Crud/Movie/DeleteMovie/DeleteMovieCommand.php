ae30f414cfb4f9a84edc790fa61b51b6466b1636
andrey-petritsa <andrey@petritsa.ru>
2021-10-21 15:47:39 +0300

    I-15 mv project symfony root folder to ../

---
+++ src/Command/Crud/Movie/DeleteMovie/DeleteMovieCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Command\Crud\Movie\DeleteMovie;
+
+use Happyr\Validator\Constraint\EntityExist;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class DeleteMovieCommand
+{
+    /**
+     * @Assert\NotBlank
+     * @EntityExist(entity="App\Domain\Booking\Entity\Movie")
+     */
+    public UuidInterface $id;
+
+    public function __construct(UuidInterface $id)
+    {
+        $this->id = $id;
+    }
+}
---
+++ src/Command/Crud/Session/DeleteSession/DeleteSessionCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Command\Crud\Session\DeleteSession;
+
+use Happyr\Validator\Constraint\EntityExist;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class DeleteSessionCommand
+{
+    /**
+     * @Assert\NotBlank
+     * @EntityExist(entity="App\Domain\Booking\Entity\Session\Session")
+     */
+    public $sessionId;
+
+    public function __construct($sessionId)
+    {
+        $this->sessionId = $sessionId;
+    }
+}
